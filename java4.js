a8c73977f1ba014e80f5313766635da156430cf8
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-02-01 00:05:55 +0500

    uyga vazifa 4

---
+++ java4.js
@@ -0,0 +1,37 @@
+// .1
+if (0) {
+    alert ('hello')
+}
+// Bunda undefined deb chiqadi
+
+// .2
+let name = "Question"
+ 
+if ( name === "Question"){
+    console.log( "What's the official name of JavaScript")
+} else if (name === "ECMAScript"){
+    console.log( "Right!");
+} else{
+    console.log( "You don't know? \" EcmaScript\"! ");
+}
+// .3
+let a = 5
+
+if ( a > 0) {
+    console.log(1)
+} else if ( a < 0) {
+    console.log(-1)
+} else if ( a === 0) {
+    console.log(0)
+}
+// .4
+let b = 2, c = 3
+
+if ( a+b < 4 ) {
+    console.log ('Below')
+} else {
+    console.log('Over')
+}
+let d = 2, e = 3
+d + e <4 ? console.log('Below')
+: console.log('Over')
