8b8d50dfaaa1da710a8749e3122694a28ea02bfd
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-02-08 12:35:53 +0500

    uyga vazifa 6

---
+++ java6.js
@@ -0,0 +1,48 @@
+//.1
+// let webbrain={
+//     name:"John",
+//     surname:"Smith"
+// }
+// webbrain.name="Pete"
+// console.log(webbrain); // bu 1-chi holat 
+// delete webbrain.name
+// console.log(webbrain); // bu oxirgi holat
+//.2
+// let schedule = {};
+// console.log(isEmpty(schedule))
+// agar bunaqa bo'lsa unda IS NOT DEFINED BERADI
+// let schedule = {
+//     time:"8:30"
+// }
+// console.log ( isEmpty(schedule));
+// umuman olib qarasak isEmpty degan qiymat bizda yuq ekan
+
+//.3
+// let salaries = {
+//     John:100,
+//     Ann:160,
+//     Pete:130,
+// }
+
+//////////////////////////// Method 1
+// let sum = 0
+// for (const key in salaries) {
+//     sum += salaries[key] 
+// }
+// console.log(`Total sum is ${sum}`);
+
+//.4
+// let menu ={
+//   width : 200,
+//   height : 300,
+//   title:"My menu"
+// }
+
+// for (const key in menu) {
+//     if (typeof menu[key] === 'number') {
+//         menu[key] = menu[key] * 2
+//     }
+// }
+// console.log(menu);
+
+
