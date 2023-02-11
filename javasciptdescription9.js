cd42ae1eccd4cb3b9478772785256a2c5d80a3e4
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-02-11 19:01:12 +0500

    strings

---
+++ javasciptdescription9.js
@@ -0,0 +1,18 @@
+// Strings
+// let a = 'webbrain'
+// let b = "webbrain"
+// let c = `webbrain`
+// shunga uxshash xar xil usullarda bersak bo'ladi 
+// lekin uchunchisida biz qator tashlasak ham bo'ladi
+// /n - new line 
+// keyin biz huddi objectga o'xshab backtick dan foydalanib biz harflardi index raqami orqali chaqirsak bo'ladi
+
+let a = `webbrain IT centre`
+// console.log(a[1]); bunda endi bizga e harfini olib keladi
+
+// console.log(a.length); bu textdi uzunligini chiqarib beradi
+
+// console.log(a[1]); bu oddiy holat 
+console.log(a.charAt(-1));
+console.log(a.charAt(1));
+console.log(a.charAt(1));
---
+++ javascriptdescription9.js
@@ -0,0 +1,33 @@
+// Strings
+// let a = 'webbrain'
+// let b = "webbrain"
+// let c = `webbrain`
+// shunga uxshash xar xil usullarda bersak bo'ladi 
+// lekin uchunchisida biz qator tashlasak ham bo'ladi
+// /n - new line 
+// keyin biz huddi objectga o'xshab backtick dan foydalanib biz harflardi index raqami orqali chaqirsak bo'ladi
+
+let a = `webbrain`
+// console.log(a[1]); bunda endi bizga e harfini olib keladi
+
+// console.log(a.length); bu textdi uzunligini chiqarib beradi
+
+// console.log(a[1]); bu oddiy holat 
+// console.log(a.at(-1));
+// console.log(a.charAt(1));
+// at bilan charAt bu ikkalasi ham indexga qarab raqamdi chiqarib beradi , lekin at da - ham 
+// ishlaydi ,lekin charAt da manfiy sonlar ishlamaydi. keyin bulardi biz stringda bersak ham ishlaydi 
+// console.log(a.toUpperCase()); bu hammasini katta harf qilib beradi
+// console.log(a.toLowerCase()); bu hammasini kichik harf qilib beradi
+
+// cosole.log(a.indexOf('bb')) desak unda manashu narsa variabledi qiymatida bormi yoki yuq o'shani tekshirib beradi agar bo'lsa 
+// o'shani indexini chiqarib beradi , agar yuq bo'lsa unda -1 deb chiqarib beradi
+// agar 100% so'zga match qiladigan bo'lsa unda 0 chiqadi
+
+// cosole.log(a.lastindexOf('bb')) desak bu oxiridan boshlab sanab bizga indexini chiqarib beradi
+
+// includes ham shu teppadagi ikkitaga o'xshaudi , lekin bu true yoki false qaytaradi
+
+// console.log(a.startsWith("+998")) desak unda bu shu raqam bilan boshlanganmi yoki boshlanmaganmi
+// shuni ifodalab beradi 
+// console.log(a.endsWith("+998")) bu shuni tugashini ifodalab beradi 
